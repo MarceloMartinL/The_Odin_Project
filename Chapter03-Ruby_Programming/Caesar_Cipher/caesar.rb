8ac4f08ad45f41fc2a88e9b54cc1233901d2100b
Marcelo Martin <marcelo18a@msn.com>
2017-09-28 19:11:05 -0300

    Added Chapter 3 and the Caesar Cipher Project

---
+++ Chapter03-Ruby_Programming/Caesar_Cipher/caesar.rb
@@ -0,0 +1,37 @@
+def caesar (string, number)
+  sentence = string
+  cipher = ""
+  sentence.scan(/./) do |letter|
+  	if letter =~ /[a-z]/
+      convertion = letter.ord + number
+      if convertion >= 97 and convertion <= 122 
+        cipher += convertion.chr
+      elsif convertion > 122
+      	convertion = 96 + (convertion - 122)
+      	cipher += convertion.chr
+      elsif convertion < 97
+      	convertion = 123 - (97 - convertion)
+      	cipher += convertion.chr
+      end
+
+    elsif letter =~ /[A-Z]/
+      convertion = letter.ord + number
+      if convertion >= 65 and convertion <= 90
+      	cipher += convertion.chr
+      elsif convertion > 90
+      	convertion = 64 + (convertion - 90)
+      	cipher += convertion.chr
+      elsif convertion < 65
+      	convertion = 91 - (65 - convertion)
+      	cipher += convertion.chr
+      end
+
+    elsif letter == " "
+      cipher += " "
+    end
+  end
+  puts cipher
+end
+
+caesar("beCas A", 3)
+
