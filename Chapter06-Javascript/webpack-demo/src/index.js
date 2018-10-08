b05de3954a9326e2d87d97800f8d323d8e21362c
Marcelo Martin <marcelo18a@msn.com>
2018-10-08 01:05:19 -0300

    webpack demo

--- Chapter06-Javascript/webpack-demo/src/index.js
+++ Chapter06-Javascript/webpack-demo/src/index.js
@@ -1,10 +1,17 @@
 import _ from 'lodash';
 import myName from './myName';
+import printMe from './print.js';
 
 function component() {
   let element = document.createElement('div');
+  let btn = document.createElement('button');
 
   element.innerHTML = myName('Marcelo');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
